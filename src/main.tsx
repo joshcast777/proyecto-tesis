0acf3e1f26199ff608508592391f21dbe0538775
Joshua Castillo <josh.cast777@outlook.com>
2024-06-11 23:50:19 -0500

    Router JSON

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import "@/styles/styles.scss";
-import { Toaster } from "sonner";
-import { BrowserRouter } from "react-router-dom";
+// import { Toaster } from "sonner";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { LoginForm } from "./views/index.ts";
+import AdminDashboard from "./views/Dashboard/AdminDashboard.tsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/auth",
+		element: <LoginForm />
+	},
+	{
+		path: "/admin-dashboard",
+		element: <AdminDashboard />
+	}
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<RouterProvider router={router} />
+		{/* <BrowserRouter>
 			<App />
 
 			<Toaster expand={true} />
-		</BrowserRouter>
+		</BrowserRouter> */}
 	</React.StrictMode>
 );
