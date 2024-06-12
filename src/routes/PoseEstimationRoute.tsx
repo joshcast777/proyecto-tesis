c36b5652dcdca3741ea3300322ed7251dde86ec3
Joshua Castillo <josh.cast777@outlook.com>
2024-06-11 22:57:26 -0500

    Ruta 1

--- src/routes/PoseEstimationRoute.tsx
+++ src/routes/PoseEstimationRoute.tsx
@@ -3,7 +3,7 @@
  */
 
 // import { authStore } from "@/store";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 // import { PrivateRoute, PublicRoute } from "./components/AccesRoutes";
 import { LoginForm } from "@/views";
 import AdminDashboard from "@/views/Dashboard/AdminDashboard";
@@ -36,7 +36,15 @@ export default function PoseEstimationRoute(): React.ReactNode {
 			<Route path="/auth" element={<LoginForm />} />
 			<Route path="/admin-dashboard" element={<AdminDashboard />} />
 
-			<Route path="" element={<Navigate to="/admin-dashboard" />} />
+			{/* <Route path="" element={<Navigate to="/admin-dashboard" />} /> */}
+			<Route
+				path="*"
+				element={
+					<div className="flex h-full w-full items-center justify-center">
+						<p className="text-2xl uppercase dark:text-white">Esta seccion aun se encuentra en desarrollo</p>
+					</div>
+				}
+			/>
 			{/* <Route path="/app" element={<Navigate to="/app/dashboard" />} />
 				<Route path="/dashboard" element={<Navigate to="/app/dashboard" />} />
 				<Route path="/companies" element={<Navigate to="/app/companies" />} />
